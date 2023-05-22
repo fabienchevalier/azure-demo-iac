830c74cba31e7e879a701731b3de3d65e0522b31
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2023-05-22 22:54:25 +0200

    Fix ansible role

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+data "azurerm_shared_image" "example" {
+  name                = "my-image"
+  gallery_name        = "my-image-gallery"
+  resource_group_name = "example-resources-imageRG"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "xxxxximageregion"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "<smallest tier>"
+  admin_username      = "<admin username>"
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id = data.azurerm_shared_image.example.id
+
+}
